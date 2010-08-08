8a835c53bbbf0dab081d300db18772d4bb338f67
Bahtiar Gadimov <mail@kalkin.de>
2010-08-08 19:37:47 +0200

    Removed gen/

--- gen/org/moo/android/filebrowser/R.java
+++ gen/org/moo/android/filebrowser/R.java
@@ -34,12 +34,17 @@ public final class R {
         public static final int webdoc=0x7f02000a;
     }
     public static final class id {
-        public static final int GridView=0x7f060000;
+        public static final int GridView=0x7f070000;
+        public static final int preferences=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int main=0x7f060000;
+    }
     public static final class string {
-        public static final int app_name=0x7f050000;
+        public static final int app_name=0x7f050001;
+        public static final int preferences=0x7f050000;
     }
 }
