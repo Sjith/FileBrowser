7cabb9a94a17cbcf28ada8b5724f406bac8fafaf
Bahtiar Gadimov <mail@kalkin.de>
2010-08-08 19:36:53 +0200

    Added Preferences Activity

---
+++ src/org/moo/android/filebrowser/Preferences.java
@@ -0,0 +1,20 @@
+package org.moo.android.filebrowser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Preferences extends Activity {
+
+	public Preferences() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
